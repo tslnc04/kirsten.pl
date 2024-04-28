0b786a798e81fe2ee821e365ee55bdd0abac01de
Kirsten Laskoski <kirsten.laskoski@gmail.com>
2024-04-27 21:59:36 -0400

    feat: updated projects, fixed robots txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()]
+  site: "https://kirsten.pl",
+  integrations: [tailwind(), robotsTxt()],
 });
